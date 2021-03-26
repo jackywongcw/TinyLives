2122ea54ebc99cbbb937eae5cdecf7ecf6d81cb9
JackyWongCW <hello@jackywongcw.com>
2021-03-26 23:48:24 +0800

    New Post class

---
+++ TinyLives/Post.swift
@@ -0,0 +1,19 @@
+//
+//  Post.swift
+//  TinyLives
+//
+//  Created by Jacky Wong on 26/3/21.
+//
+
+import Foundation
+
+class Post {
+    
+    var id = ""
+    var downloadable : Bool? = false
+    var expiryDate = ""
+    var message = ""
+    var title = ""
+    var uploadedDate = ""
+    
+}
